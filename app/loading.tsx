da1cf66e6dd452795d922cad11271e98e94f31a7
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-06 21:21:32 -0300

    feat: nova animação de loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,9 @@
-import { Skeleton } from "@/app/_components/ui/skeleton";
+import LoaderAnimate from "./_components/_loader/loaderAnimate";
 
 const AnimateLoader = () => {
   return (
-    <div className="grid grid-cols-3 gap-5 p-5">
-      {Array.from({ length: 9 }).map((_, index) => (
-        <div className="justify-self-center" key={index}>
-          <Skeleton className="h-[100px] w-[100px] rounded-md bg-muted-foreground" />
-        </div>
-      ))}
+    <div className="flex h-[90vh] w-full items-center justify-center">
+      <LoaderAnimate />
     </div>
   );
 };
