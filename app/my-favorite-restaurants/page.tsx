e70d2266d9dcf959a1c55424a0b613e53226066c
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-07 11:04:25 -0300

    feat: responsividade da pagina dos restaurantes favoritos

--- app/my-favorite-restaurants/page.tsx
+++ app/my-favorite-restaurants/page.tsx
@@ -4,6 +4,8 @@ import { authOptions } from "../_lib/auth";
 import { notFound } from "next/navigation";
 import Header from "../_components/header";
 import RestaurantItem from "../_components/restaurant-item";
+import ButtonBack from "../_components/buttonBack";
+import Link from "next/link";
 
 const MyFavoriteRestaurants = async () => {
   const session = await getServerSession(authOptions);
@@ -25,21 +27,33 @@ const MyFavoriteRestaurants = async () => {
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">Restaurantes Favoritos</h2>
+        <div className="mb-6 flex flex-col items-start gap-6 px-2">
+          <ButtonBack />
+          <h2 className="text-lg font-semibold">Restaurantes Favoritos</h2>
+        </div>
+
         <div className="flex w-full flex-col gap-6">
           {userFavoriteRestaurants.length > 0 ? (
             userFavoriteRestaurants.map(({ restaurant }) => (
               <RestaurantItem
                 key={restaurant.id}
                 restaurant={restaurant}
-                className="min-w-full max-w-full"
+                className="mx-auto w-full max-w-[700px]"
                 userFavoriteRestaurants={userFavoriteRestaurants}
               />
             ))
           ) : (
-            <h3 className="font-medium">
-              Você ainda não marcou nenhum restaurante como favorito.
-            </h3>
+            <div className="flex flex-col items-center gap-5">
+              <h3 className="font-medium">
+                Você ainda não marcou nenhum restaurante como favorito.
+              </h3>
+              <Link
+                href="/restaurants/recommended"
+                className="text-primary underline"
+              >
+                Dê uma olhada nos restaurantes
+              </Link>
+            </div>
           )}
         </div>
       </div>
