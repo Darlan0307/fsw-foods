0cb6838c510c17bd1631e74b8648c7124267bd74
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-09 13:54:15 -0300

    refactor: refatoranto o mode de como renderizar os links do rodape

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -6,6 +6,27 @@ import { Github, Instagram, Linkedin } from "lucide-react";
 const Footer = () => {
   let date = new Date();
 
+  const social = [
+    {
+      id: crypto.randomUUID,
+      name: "GitHub",
+      icon: <Github size={35} />,
+      link: "https://github.com/Darlan0307",
+    },
+    {
+      id: crypto.randomUUID,
+      name: "LinkedIn",
+      icon: <Linkedin size={35} />,
+      link: "https://www.linkedin.com/in/darlan-martins-8a7956259/",
+    },
+    {
+      id: crypto.randomUUID,
+      name: "Instagram",
+      icon: <Instagram size={35} />,
+      link: "https://www.instagram.com/darlan_sw_/",
+    },
+  ];
+
   return (
     <footer className="mt-10 flex min-h-[60vh] flex-col justify-around bg-primary px-10 sm:min-h-[50vh] md:min-h-[40vh]">
       <div className="flex flex-col justify-center gap-3 sm:flex-row sm:items-center sm:gap-6">
@@ -33,48 +54,23 @@ const Footer = () => {
             Copyright @{date.getFullYear()}
           </p>
           <div className="flex items-center gap-5 ">
-            <Link
-              href="https://github.com/Darlan0307"
-              className="relative "
-              target="_blank"
-            >
-              <Github
-                color="#979595"
-                size={35}
-                className="peer transition-all hover:scale-110"
-              />
-              <span className="absolute -top-20 left-[50%] -translate-x-[50%]  font-bold text-[#979595] opacity-0 transition-all peer-hover:-top-8 peer-hover:opacity-100">
-                GitHub
-              </span>
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/darlan-martins-8a7956259/"
-              className="relative"
-              target="_blank"
-            >
-              <Linkedin
-                color="#55bdf5"
-                size={35}
-                className="peer transition-all hover:scale-110"
-              />
-              <span className="absolute -top-20 left-[50%] -translate-x-[50%]  font-bold text-[#55bdf5] opacity-0 transition-all peer-hover:-top-8 peer-hover:opacity-100">
-                LinkedIn
-              </span>
-            </Link>
-            <Link
-              href="https://www.instagram.com/darlan_sw_/"
-              className="relative"
-              target="_blank"
-            >
-              <Instagram
-                color="#ff55be"
-                size={35}
-                className="peer transition-all hover:scale-110"
-              />
-              <span className="absolute -top-20 left-[50%] -translate-x-[50%]  font-bold text-[#ffafe0] opacity-0 transition-all peer-hover:-top-8 peer-hover:opacity-100">
-                Instagram
-              </span>
-            </Link>
+            {social.map((link) => (
+              <Link
+                href={link.link}
+                className="relative"
+                target="_blank"
+                key={link.id.toString()}
+              >
+                <div className="peer text-white transition-all hover:scale-110">
+                  {link.icon}
+                </div>
+                <span
+                  className={`absolute -top-20 left-[50%] -translate-x-[50%]  font-bold text-white opacity-0 transition-all peer-hover:-top-8 peer-hover:opacity-100`}
+                >
+                  {link.name}
+                </span>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
